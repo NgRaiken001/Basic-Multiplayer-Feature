e7ccc857a21f3930bce8d6b3c16e28baddca709e
Ashutosh Pandey <111453787+NgRaiken001@users.noreply.github.com>
2025-05-13 11:44:59 +0530

    Update and rename New Text Document (5).txt to MultiplayerPlayerController.h

---
+++ MultiplayerPlayerController.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MultiplayerPlayerController.generated.h"
+
+UCLASS()
+class MYGAME_API AMultiplayerPlayerController : public APlayerController
+{
+    GENERATED_BODY()
+};
