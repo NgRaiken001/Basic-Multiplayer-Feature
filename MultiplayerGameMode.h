1b0ea00d746308a36699660ed16e7c8d889e52ec
Ashutosh Pandey <111453787+NgRaiken001@users.noreply.github.com>
2025-05-13 11:44:06 +0530

    Update and rename New Text Document (4).txt to MultiplayerGameMode.h

---
+++ MultiplayerGameMode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiplayerGameMode.generated.h"
+
+UCLASS()
+class MYGAME_API AMultiplayerGameMode : public AGameModeBase
+{
+    GENERATED_BODY()
+
+public:
+    AMultiplayerGameMode();
+};
